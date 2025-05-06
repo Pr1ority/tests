017fc4f10300956819babb334696af6eef6a5927
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2025-05-06 23:30:05 +0300

    add login page

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    login_link = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocatots:
+    login_form = (By.ID, 'login_form')
+    register_form = (By.ID, 'register_form')
---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocatots
+
+
+class LoginPage(BasePage):
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, (
+            'Неккоректная ссылка, должна быть ссылка на логин'
+        )
+
+    def should_be_login_form(self):
+        assert self.is_element_present(
+            *LoginPageLocatots.login_form
+        ), 'Отсутствует форма логина'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(
+            *LoginPageLocatots.register_form
+        ), 'Отсутствует форма регистрации'
